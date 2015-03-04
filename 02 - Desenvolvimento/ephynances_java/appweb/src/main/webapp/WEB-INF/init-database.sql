dadf63b66276c84f4b18e366978e7ca6c77a80f2
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2015-03-04 14:41:27 -0300

    Adicionando dados iniciais para regiões, estados e city orgãoes

--- 02 - Desenvolvimento/ephynances_java/appweb/src/main/webapp/WEB-INF/init-database.sql
+++ 02 - Desenvolvimento/ephynances_java/appweb/src/main/webapp/WEB-INF/init-database.sql
@@ -8,13 +8,34 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 USE `physis_ephynances`;
 
--- User --
+-- Users --
 insert into `user`(email, name, password, cpf, phone, cell_phone, max_sales_amount, profile_rule, delete_date) 
 values ('admin@physisbrasil.com.br', 'Administrador', 'c0859afbb1427c8ead10835ae456db9', '84093587515', '(73) 9119-2425', '(73) 9119-2425', 0, 'Administrador', null);
 
--------- Configuration ---------
+-------- Configurations ---------
 insert into configuration(smtp_server, user_name, password, smtp_port, email)
 values ('smtp.gmail.com', 'manoel.carvalho.neto', 'mxthomasmx', 587, 'manoel.carvalho.neto@gmail.com');
+
+--- Regions ---
+insert into region(name, acronym)
+values ('Norte', 'N'),
+('Nordeste', 'NE'), 
+('Sudeste', 'SE'), 
+('Centro-Oeste', 'CO'),
+('Sul', 'S');
+
+--- States ---
+insert into `state`(region_id, name, acronym)
+values (1, 'Acre', 'AC'), (1, 'Amapá', 'AP'), (1, 'Amazonas', 'AM'), (1, 'Pará', 'PA'), (1, 'Rondônia', 'RO'), (1, 'Roraima', 'RR'), (1, 'Tocantins', 'TO'),
+(2, 'Alagoas', 'AL'), (2, 'Bahia', 'BA'), (2, 'Ceará', 'CE'), (2, 'Maranhão', 'MA'), (2, 'Paraíba', 'PB'), (2, 'Piauí', 'PI'), (2, 'Pernambuco', 'PE'), (2, 'Rio Grande do Norte', 'RN'), (2, 'Sergipe', 'SE'),
+(3, '', ''), (3, '', ''), (3, '', ''),  (3, '', ''),
+(4, '', ''), (4, '', ''), (4, '', ''), (4, '', ''),
+(5, '', ''), (5, '', ''), (5, '', '');
+
+--- CityOrgans ---
+insert into city_organ()
+values (),
+();
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
