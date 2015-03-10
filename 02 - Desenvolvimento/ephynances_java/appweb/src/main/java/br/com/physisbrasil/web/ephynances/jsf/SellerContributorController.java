eb248881801ce5691473c9d8fef7cced921717c8
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2015-03-10 14:44:23 -0300

    Adicionada tela para listar colaboradores do vendedor Filtro para botão de colaboradores de vendedor apenas nos vendedores Método para carregar a lista de usuários do tipo colaborador

--- 02 - Desenvolvimento/ephynances_java/appweb/src/main/java/br/com/physisbrasil/web/ephynances/jsf/SellerContributorController.java
+++ 02 - Desenvolvimento/ephynances_java/appweb/src/main/java/br/com/physisbrasil/web/ephynances/jsf/SellerContributorController.java
@@ -1,7 +1,9 @@
 package br.com.physisbrasil.web.ephynances.jsf;
 
 import br.com.physisbrasil.web.ephynances.ejb.SellerContributorBean;
+import br.com.physisbrasil.web.ephynances.ejb.UserBean;
 import br.com.physisbrasil.web.ephynances.model.SellerContributor;
+import br.com.physisbrasil.web.ephynances.model.User;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -20,13 +22,31 @@ public class SellerContributorController extends BaseController {
     @EJB
     private SellerContributorBean sellerContributorBean;
     private List<SellerContributor> sellerContributors;
-    
+
+    @EJB
+    private UserBean userBean;
+    private User seller;
+    private List<User> contributors;
+
     @PostConstruct
     public void init() {
         if (sellerContributors == null) {
             sellerContributors = new ArrayList<SellerContributor>();
             setSellerContributors(sellerContributorBean.findAll());
         }
+
+        seller = (User) getFlash("seller");
+        if (seller == null) {
+            seller = new User();
+        }
+
+        contributors = (List<User>) getFlash("contributors");
+        if (contributors == null) {
+            contributors = new ArrayList<User>();
+        }
+
+        putFlash("seller", null);
+        putFlash("contributors", null);
     }
 
     public List<SellerContributor> getSellerContributors() {
@@ -36,10 +56,51 @@ public class SellerContributorController extends BaseController {
     public void setSellerContributors(List<SellerContributor> sellerContributors) {
         this.sellerContributors = sellerContributors;
     }
-    
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public List<User> getContributors() {
+        return contributors;
+    }
+
+    public void setContributors(List<User> contributors) {
+        this.contributors = contributors;
+    }
+
+    public void edit(User user) {
+
+    }
+
+    public void view(User user) {
+
+    }
+
+    public void active_desactive(Long id) {
+
+    }
+
+    public void delete(Long id) {
+
+    }
+
     public String listBySeller(Long userId) {
         sellerContributors = sellerContributorBean.findBySellerId(userId);
-        
+        seller = userBean.find(userId);
+
+        contributors = new ArrayList<User>();
+        for (SellerContributor contribs : sellerContributors) {
+            contributors.add(userBean.find(contribs.getContributor().getId()));
+        }
+
+        putFlash("seller", seller);
+        putFlash("contributors", contributors);
+
         return "/sellerContributor/list";
     }
 }
