0e748c5309ab3335ff388ea79b3f6cff70d3848d
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2015-03-31 17:53:24 -0300

    Adicionando o contrato no banco de dados IDs: 69

--- 02 - Desenvolvimento/ephynances_java/appweb/src/main/java/br/com/physisbrasil/web/ephynances/jsf/AgreementController.java
+++ 02 - Desenvolvimento/ephynances_java/appweb/src/main/java/br/com/physisbrasil/web/ephynances/jsf/AgreementController.java
@@ -1,7 +1,10 @@
 package br.com.physisbrasil.web.ephynances.jsf;
 
 import br.com.physisbrasil.web.ephynances.ejb.AgreementBean;
+import br.com.physisbrasil.web.ephynances.ejb.UserBean;
 import br.com.physisbrasil.web.ephynances.model.Agreement;
+import br.com.physisbrasil.web.ephynances.model.SellerContributor;
+import br.com.physisbrasil.web.ephynances.model.User;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -23,6 +26,12 @@ public class AgreementController extends BaseController {
     @EJB
     private AgreementBean agreementBean;
     private List<Agreement> agreements;
+    private List<Agreement> filteredAgreements;
+    private Agreement agreement;
+
+    @EJB
+    private UserBean userBean;
+    private User agreementUser;
 
     @PostConstruct
     public void init() {
@@ -30,10 +39,75 @@ public class AgreementController extends BaseController {
             agreements = new ArrayList<Agreement>();
             setAgreements(agreementBean.findAll());
         }
+
+        if ((User) getFlash("userAgreement") != null) {
+            agreementUser = (User) getFlash("userAgreement");
+        } else {
+            if (agreementUser == null) {
+                agreementUser = new User();
+            }
+        }
+
+        if ((Agreement) getFlash("agreement") != null) {
+            agreement = (Agreement) getFlash("agreement");
+        } else {
+            if (agreement == null) {
+                agreement = new Agreement();
+            }
+        }
+        
+        if ((List<Agreement>) getFlash("agreements") != null) {
+            filteredAgreements = (List<Agreement>) getFlash("agreements");
+        } else {
+            if (filteredAgreements == null) {
+                filteredAgreements = new ArrayList<Agreement>();
+            }
+        }
     }
 
-    public void listByResponsibleId(int id) {
+    public void getListFilteredBySeller(Long userId) {
+        User tempUser = userBean.find(userId);
+        if (tempUser != null) {
+            if (tempUser.getProfileRule().equals(User.getRULER_CONTRIBUTOR())) {
+                filteredAgreements = tempUser.getSellers().get(0).getSeller().getAgreements();
+            } else {
+                filteredAgreements = tempUser.getAgreements();
+            }
+            
+            putFlash("agreements", getFilteredAgreements());
+        }
+    }
+    
+    public String startAgreement(Long userId) {
+        User tempUser = userBean.find(userId);
+        if (tempUser != null) {
+            if (tempUser.getProfileRule().equals(User.getRULER_CONTRIBUTOR())) {
+                setAgreementUser(tempUser);
+            } else {
+                setAgreementUser(tempUser.getSellers().get(0).getSeller());
+            }
+            
+            putFlash("userAgreement", getAgreementUser());
+            return "create";
+        }
+        
+        return "list";
+    }
 
+    public Agreement getAgreement() {
+        return agreement;
+    }
+
+    public void setAgreement(Agreement agreement) {
+        this.agreement = agreement;
+    }
+
+    public User getAgreementUser() {
+        return agreementUser;
+    }
+
+    public void setAgreementUser(User agreementUser) {
+        this.agreementUser = agreementUser;
     }
 
     public List<Agreement> getAgreements() {
@@ -43,9 +117,21 @@ public class AgreementController extends BaseController {
     public void setAgreements(List<Agreement> agreements) {
         this.agreements = agreements;
     }
-    
+
+    public List<Agreement> getFilteredAgreements() {
+        return filteredAgreements;
+    }
+
+    public void setFilteredAgreements(List<Agreement> filteredAgreements) {
+        this.filteredAgreements = filteredAgreements;
+    }
+
     public String formatDate(Date dateToFormat) {
         DateFormat outputFormatter = new SimpleDateFormat("dd/MM/yyyy");
         return outputFormatter.format(dateToFormat);
+    }
+    
+    public List<String> getAgreementTypes() {
+        return Agreement.getAGREEMENT_TYPES();
     }
 }
