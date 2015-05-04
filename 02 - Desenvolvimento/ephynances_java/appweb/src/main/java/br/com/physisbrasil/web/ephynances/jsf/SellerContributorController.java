3bb6af62bb4fa3b79db6fcb9e68cafced05b2001
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2015-05-04 09:21:36 -0300

    Correções nos colaboradores para refletir as mudanças no objeto usuário

--- 02 - Desenvolvimento/ephynances_java/appweb/src/main/java/br/com/physisbrasil/web/ephynances/jsf/SellerContributorController.java
+++ 02 - Desenvolvimento/ephynances_java/appweb/src/main/java/br/com/physisbrasil/web/ephynances/jsf/SellerContributorController.java
@@ -227,6 +227,8 @@ public class SellerContributorController extends BaseController {
                     tempUser.setNumberAutority(0);
                     tempUser.setIsVerified(false);
                     tempUser.setPassword(Criptografia.criptografar(tempUser.getCpf() + String.valueOf(System.currentTimeMillis())));
+                    tempUser.setSystemEsicar(seller.getSystemEsicar());
+                    tempUser.setProfileRule(User.getRULER_CONTRIBUTOR());
                     userBean.create(tempUser);
 
                     //Criando o relacionamento
@@ -246,7 +248,8 @@ public class SellerContributorController extends BaseController {
                     activationBean.clearCache();
 
                     Configuration config = configurationBean.findAll().get(0);
-                    Utils.sendEmail(tempUser.getEmail(), tempUser.getName(), "<html><body><a href='http://192.168.0.105:8080/ephynances/activation/active.xhtml?token=" + activation.getToken() + "'>Ativar Ephynances</a></body></html>", config.getSmtpServer(), config.getEmail(), "Ativação Ephynances", config.getUserName(), config.getPassword(), config.getSmtpPort(), "Ativador Physis Ephynances");
+                    Utils.sendEmail(tempUser.getEmail(), tempUser.getName(), String.format("<html><div align='center' style=\"background-image: url('%s'); width: 542px; height: 549px; margin-left: 250px;\"><a href='http://esicar.physisbrasil.com.br:8080/ephynances/activation/active.xhtml?token=%s'><img src='%s' width='331' height='40' style=\"margin-top: 400px;\"/></a></div></html>", "http://esicar.physisbrasil.com.br:8080/ephynances/resources/img/bg_ativar_1.png", activation.getToken(), "http://esicar.physisbrasil.com.br:8080/ephynances/resources/img/bt_ativar_1.png"),
+                            config.getSmtpServer(), config.getEmail(), "Ativação Ephynances", config.getUserName(), config.getPassword(), config.getSmtpPort(), "Ativador Physis Ephynances");
 
                     
                     JsfUtil.addSuccessMessage("Colaborador adicionado com sucesso!");
