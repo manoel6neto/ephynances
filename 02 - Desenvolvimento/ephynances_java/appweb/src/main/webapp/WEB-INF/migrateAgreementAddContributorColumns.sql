2830b25c14c068110e42300480107cce9b724dd4
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2015-05-12 09:58:26 -0300

    Adicionada migração para o banco de dados

---
+++ 02 - Desenvolvimento/ephynances_java/appweb/src/main/webapp/WEB-INF/migrateAgreementAddContributorColumns.sql
@@ -0,0 +1,5 @@
+UPDATE TABLE `agreement`
+ADD `contributor_email` varchar(200) DEFAULT NULL,
+ADD `contributor_name` varchar(200) DEFAULT NULL,
+ADD `contributor_position` varchar(200) DEFAULT NULL;
+
