780683184f7c48af22cc17a90b2fcd0e966845de
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2015-05-12 10:41:23 -0300

    Sql corrigido

--- 02 - Desenvolvimento/ephynances_java/appweb/src/main/webapp/WEB-INF/migrateAgreementAddContributorColumns.sql
+++ 02 - Desenvolvimento/ephynances_java/appweb/src/main/webapp/WEB-INF/migrateAgreementAddContributorColumns.sql
@@ -1,5 +1,5 @@
-UPDATE TABLE `agreement`
-ADD `contributor_email` varchar(200) DEFAULT NULL,
-ADD `contributor_name` varchar(200) DEFAULT NULL,
-ADD `contributor_position` varchar(200) DEFAULT NULL;
+ALTER TABLE `agreement`
+ADD COLUMN `contributor_email` varchar(200) DEFAULT NULL,
+ADD COLUMN `contributor_name` varchar(200) DEFAULT NULL,
+ADD COLUMN `contributor_position` varchar(200) DEFAULT NULL;
 
